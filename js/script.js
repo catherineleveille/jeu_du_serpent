837e2da21dc24fb7cf42f885a9ed7adc9d5c3c6d
Catherine Leveille <leveillec@hotmail.ca>
2020-11-20 09:17:47 -0500

    introduction

--- js/script.js
+++ js/script.js
@@ -4,9 +4,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
     class Jeu {
         //Définit toutes les caractéristiques du jeu
         //Lorsqu'on crée une nouvelle classe, la fonction contructor permet d'initialiser des variables
-        constructor() {
+        //ici on dit au constructor qu'il va recevoir 2 paramètres
+        constructor(_idSvg, _idPointage) {
+            //ces paramètres existent seulement le temps de la fonction dans laquelle ils sont passées
             console.log("Création du jeu");
+            //fonctionnalité de snap svg
+            this.s = Snap(_idSvg);
+            this.sortiePointage = document.querySelector(_idPointage);
+            this.grandeurCarre = 20;
+            this.grandeurGrille = 15;
 
+
+        }
+
+        //3 fonctions importantes pour la création du jeu
+        nouvellePartie() {
+            //faire les nouvelles parties
+            this.afficherPointage(1);
+            this.pomme=new Pomme();
+            this.serpent= new Serpent();
+
+        }
+
+        finPartie() {
+
+        }
+        //Ici, on lui passe un chiffre pour afficher le pointage et on le met dans le span du html
+        afficherPointage(_lePointage) {
+            this.sortiePointage.innerHTML=_lePointage;
 
         }
     }
@@ -16,15 +41,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
     class Serpent {
         constructor() {
             console.log("Création du serpent")
+
+
         }
     }
 
     //La pomme
     class Pomme {
-    constructor() {
-        console.log("Création de la pomme")
+        constructor() {
+            console.log("Création de la pomme")
+        }
     }
-    }
+
     //création d'une nouvelle partie
-    var unePartie= new Jeu();
+    var unePartie = new Jeu("#jeu", "#pointage");
+    //ici, on appelle le constructor
 });
