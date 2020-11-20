866f417e7a5ab00c210c9de3bd5300810148d836
Catherine Leveille <leveillec@hotmail.ca>
2020-11-20 08:59:47 -0500

    début du jeu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 //Le jeu
 
+    class Jeu {
+        //Définit toutes les caractéristiques du jeu
+        //Lorsqu'on crée une nouvelle classe, la fonction contructor permet d'initialiser des variables
+        constructor() {
+            console.log("Création du jeu");
+
+
+        }
+    }
+
+
+    //Le serpent
+    class Serpent {
+        constructor() {
+            console.log("Création du serpent")
+        }
+    }
+
+    //La pomme
+    class Pomme {
+    constructor() {
+        console.log("Création de la pomme")
+    }
+    }
+    //création d'une nouvelle partie
+    var unePartie= new Jeu();
 });
